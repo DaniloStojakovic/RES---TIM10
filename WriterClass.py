e86a185534960148263ae3dae2c8ff09987ef62c
Milos Pantelic <milosallenpantelic@gmail.com>
2022-06-10 16:54:38 +0200

    Create WriterClass.py
    celokupna klasa za Writer-a

---
+++ WriterClass.py
@@ -0,0 +1,20 @@
+import DataSets
+
+class WriterCl:
+  def __init__(self, Code, Value):
+    self.code= Code
+    self.value = Value
+
+
+
+
+
+
+#w1 = WriterCl("ANALOG", 23)
+
+#print(lb1.code)
+#print(lb1.value)
+
+#listItem = [w1]
+
+#print(listItem[0].value)
