fd451885d891f0f169ccd9907b48bd3425c2e364
Milos Pantelic <milosallenpantelic@gmail.com>
2022-06-10 16:49:57 +0200

    Create DataSets.py
    Napravljeni DataSet-ovi po Code-ovima

---
+++ DataSets.py
@@ -0,0 +1,15 @@
+#from dataclasses import dataclass
+import Code_Names
+
+DataSet1 = Code_Names.codeNames[0], Code_Names.codeNames[1]
+DataSet2 = Code_Names.codeNames[2], Code_Names.codeNames[3]
+DataSet3 = Code_Names.codeNames[4], Code_Names.codeNames[5]
+DataSet4 = Code_Names.codeNames[6], Code_Names.codeNames[7]
+
+
+DataSets =	{
+  1 : DataSet1,
+  2 : DataSet2,
+  3 : DataSet3,
+  4 : DataSet4
+}
