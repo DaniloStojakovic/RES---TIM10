28bd7771ab7863bd9befb6f363a0f0e9face8de4
Milos Pantelic <milosallenpantelic@gmail.com>
2022-06-10 16:55:13 +0200

    Create Writer.py
    pocetna logika za writer

---
+++ Writer.py
@@ -0,0 +1,32 @@
+from operator import truediv
+import WriterClass
+
+
+
+while True:
+
+    print("Izaberite jednu od opcija: \n 1. Upis novih podataka \n 2. Paljenje i gasenje workera \n 3. Izlaz \n ")
+
+    izbor = int(input())
+
+    if izbor == 1:
+        #ovde radimo upis novih podataka
+        print("1")
+    elif izbor == 2:
+        #ovde radimo paljenje i gasenje
+        print("2")
+    elif izbor == 3:
+        
+        print("3")
+        break;
+    else:
+        print("Izaberite redni broj zeljene opcije(od 1 do 3)!\n\n")
+
+
+
+
+
+
+
+
+
