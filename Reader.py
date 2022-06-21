5ef9631347f86b83e63cd38a050afbd5b38a9cfb
djufa762x39 <simodespotovic64@gmail.com>
2022-06-21 16:19:38 +0200

    Update Reader.py

--- Reader.py
+++ Reader.py
@@ -9,84 +9,89 @@ sys.path.append('../')
 from Reader.CreateQuery import get_query
 from _thread import start_new_thread
 
+workerAddress = "127.0.0.1"
+workerPort = 55000
+workerHost = (workerAddress, workerPort)
 
-ReceiveHost = "127.0.0.1" 
-ReceivePort = 40000
+readerAddress = "127.0.0.1" 
+readerPort = 33000
+readerHost = (readerAddress, readerPort)
 
-HistoricalHost="127.0.0.1"
-HistoricalPort=60000
-
-def get_from_historical(string):
-    sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (HistoricalHost, HistoricalPort)
-    print("connecting to Historical on address " + str(HistoricalHost)+":"+str(HistoricalPort)+"\n")
-    sock.connect(server_address)
+def getFromWorker(string):
+    workerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"connecting to Worker on [ADDR:PORT] : [{str(workerAddress)}:{str(workerPort)}]")
+    logger.info(f"connecting to Worker on [ADDR:PORT] : [{str(workerAddress)}:{str(workerPort)}]")
+    workerSocket.connect(workerHost)
 
     try:
-        sock.send(pickle.dumps(string.encode("utf-8")))
-        reply = sock.recv(1024)
-        sock.close()
+        workerSocket.send(pickle.dumps(string.encode("utf-8")))
+        reply = workerSocket.recv(1024)
+        workerSocket.close()
         print(reply.decode("utf-8"))
+        logger.info(reply.decode("utf-8"))
         return reply
 
     except Exception as e:
-        sock.close()
+        workerSocket.close()
         print(e)
+        logger.error(e)
         return "ERROR"
 
 
 
-def get_params(data):
-    option=data.split(',')[0]
-    parameter=data.split(',')[1]
-    parameter=parameter[1:]
+def getParameters(data):
+    option = data.split(',')[0]
+    parameter = data.split(',')[1]
+    parameter = parameter[1:]
     return option,parameter
 
-def multi_threaded_connection(connection): 
+def multiThreadedConnection(connection): 
     while True:
         try:
             data = connection.recv(1024)
             if not data:
                 break
-            data=(data.decode("utf-8"))                                                                     
-            option,parameter=get_params(data)
-            string=get_query(option,parameter)
-            reply=get_from_historical(string)
+            data = (data.decode("utf-8"))                                                                     
+            option,parameter = getParameters(data)
+            string = getQuery(option,parameter)
+            reply = getFromWorker(string)
             connection.sendall(reply)
         except Exception as e:
             print(e)
-        
+            logger.error(e)
       
-def start_reader():
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.bind((ReceiveHost, ReceivePort))
+def startReader():
+    readerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    readerSocket.bind(readerHost)
     print("Reader started!")
-    while s:
-        s.listen()
-        conn, addr = s.accept()
+    logger.info("Reader started!")
+    while readerSocket:
+        readerSocket.listen()
+        conn, addr = readerSocket.accept()
         print(f"Connected by {addr}")
-        start_new_thread(multi_threaded_connection, (conn, ))
+        logger.info(f"Connected by {addr}")
+        start_new_thread(multiThreadedConnection, (conn, ))
 
 if __name__ == '__main__':
-    start_new_thread(start_reader())
+    start_new_thread(startReader())
     x = input()
     while x != "x":
         x = input()
 
 
-def get_query(option,parameter):
+def getQuery(option,parameter):
     string=""
-    if option== "1":
-        string= get_all()
-    elif option== "2":
-        string =get_by_month(parameter)
-    elif option== "3":
-        string =get_by_client_id(parameter)
-    elif option== "4":
-        string=get_by_city(parameter)
-    elif option== "5":
+    if option == "1":
+        string = get_all()
+    elif option == "2":
+        string = get_by_month(parameter)
+    elif option == "3":
+        string = get_by_client_id(parameter)
+    elif option == "4":
+        string = get_by_city(parameter)
+    elif option == "5":
         string = get_by_power_consumption_above(parameter)
-    elif option== "6":
+    elif option == "6":
         string = get_by_power_consumption_below(parameter)
 
     return string
