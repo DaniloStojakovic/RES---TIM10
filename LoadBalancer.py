d5b2fdb8aa64bf69c387bb623820958545651e2e
DaniloStojakovic <fqwdqdwqd@gmail.com>
2022-06-22 01:44:50 +0200

    Popravka loadbalancer-a

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -5,6 +5,7 @@ import random
 from itertools import cycle
 import pickle
 from WriterClass import WriterClass
+from Description import Description
 
 SERVER_POOL = [('localhost', 4000), ('localhost', 5000), ('localhost', 6000), ('localhost', 7000)]
 
@@ -12,6 +13,21 @@ ITER = cycle(SERVER_POOL)
 def round_robin(iter):
     return next(iter)
 
+def which_dataset(code):
+        if code == "CODE_ANALOG" or code == "CODE_DIGITAL":
+            dataset = 1
+        if code == "CODE_CUSTOM" or code == "CODE_LIMITSET":
+            dataset = 2
+        if code == "CODE_SINGLENOE" or code == "CODE_MULTIPLENODE":
+            dataset = 3
+        if code == "CODE_CONSUMER" or code == "CODE_SOURCE":
+            dataset = 4
+        return dataset
+
+WorkerSocket = socket.socket()
+host2 = 'localhost'
+port2 = 6969
+WorkerSocket.connect((host2, port2))
 
 class LoadBalancer(object):
 
@@ -33,22 +49,32 @@ class LoadBalancer(object):
         print ('='*40+'flow start'+'='*39)
         print('Client connected: %s', client_addr)
 
+        ItemList = []
+        brojItema = 0
+
         while True:
-            #server_ip, server_port = self.select_server(SERVER_POOL, self.algorithm)
-            #ss_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #ss_socket.connect((server_ip, server_port))
             data = client_socket.recv(4096)
-            #self.on_recv(client_socket, data)
-            #ss_socket.send(data)
             data1 = pickle.loads(data)
             if not data1:
                 # if data is not received break
                 break
             item = WriterClass(data1.code, data1.value)
             print(f" code:{item.code}  lista vrednost: {item.value}\n")
-            # else:
-            #     self.on_close(client_socket)
-            #     break
+            if item.code == 'ON' or item.code == 'OFF':
+                print('a')
+            else:
+                brojItema = brojItema + 1
+                ItemList.append(item)
+                dataset = which_dataset(item.code)
+                Desc = Description(brojItema, ItemList, dataset)
+                poruka =Desc
+                zaSlanje = pickle.dumps(poruka)
+                WorkerSocket.send(zaSlanje)
+                
+
+                
+
+
 
         while True:
             read_list, write_list, exception_list = select.select(self.sockets, [], [])
@@ -97,14 +123,10 @@ class LoadBalancer(object):
 
     def on_recv(self, sock, data):
         print ('recving packets: %-20s ==> %-20s, data: %s') % (sock.getpeername(), sock.getsockname(), [data])
-        # data can be modified before forwarding to server
-        # lots of add-on features can be added here
-        #remote_socket = self.flow_table[sock]
-        #remote_socket.send(data)
-        #print ('sending packets: %-20s ==> %-20s, data: %s') % (remote_socket.getsockname(), remote_socket.getpeername(), [data])
+    
 
     def on_close(self, sock):
-        #print ('client %s has disconnected') % (sock.getpeername(),)
+        
         print ('='*41+'flow end'+'='*40)
 
         ss_socket = self.flow_table[sock]
@@ -126,8 +148,9 @@ class LoadBalancer(object):
         else:
             raise Exception('unknown algorithm: %s' % algorithm)
 
-try:
-    LoadBalancer('localhost', 5555, 'round robin').start()
-except KeyboardInterrupt:
-    print ("Ctrl C - Stopping load_balancer")
-    sys.exit(1)
+if __name__ == '__main__':
+    try:
+        LoadBalancer('localhost', 5555, 'round robin').start()
+    except KeyboardInterrupt:
+        print ("Ctrl C - Stopping load_balancer")
+        sys.exit(1)
