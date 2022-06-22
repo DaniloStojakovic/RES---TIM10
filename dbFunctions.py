69396949de83b1e0a1ffc3187407023786003228
kmh00 <dragoslavsimic8@mail.com>
2022-06-22 02:07:06 +0200

    Update on dbFunctuons
    adjusted functinons for worker

--- dbFunctions.py
+++ dbFunctions.py
@@ -2,28 +2,17 @@ import sqlite3
 from datetime import datetime
 
 class DBFunctions:
-
-   
-    def dbConnect(self,dataset):
-        try:
-            self.connection = sqlite3.connect("database.db")
-        except:
-            return False
-        return True
+    
 
     def createTable(self,dataset):
-        try:
-            self.connection.execute(f"""
-                CREATE TABLE dataset{dataset}
-                (
-                    id INT PRIMARY KEY NOT NULL,
-                    code TEXT NOT NULL,
-                    value INT NOT NULL,
-                    timestamp TEXT NOT NULL
-                )
-                """)
-        except:
-            pass
+        
+            con = sqlite3.connect('database.db')
+            cur = con.cursor()
+            self.connection.execute(f"""create table DATASET_{dataset}(code text not null,
+            value int not null,
+            date_created timestamp default current_timestamp,
+            constraint DATASET_{dataset}_CH check (DATASET_{dataset}.value>=0))""")
+        
 
     def Exist(self,id,dataset):
         retVal = self.connection.execute(f"""
@@ -35,48 +24,31 @@ class DBFunctions:
             return True
         return False
 
-    def update(self,id,code,value,dataset):
-        timestamp = datetime.now()
-        self.connection.execute(f"""
-            UPDATE dataset{dataset}
-            SET code = "{code}", value = {value}, timestamp = "{timestamp}"
-            WHERE id = {id}
-            """)
-        self.connection.commit()
+   
 
-    def getValue(self,id,dataset):
-        retVal = self.connection.execute(f"""
-            SELECT value
-            FROM dataset{dataset}
-            WHERE id = {id}
-            """)
-        value,=retVal.fetchall()[0]
-        return value
+    def GetLastValue (self, dataset, code):
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
 
-    def getWithCode(self,code,dataset):
-        retVal = self.connection.execute(f"""
-            SELECT *
-            FROM dataset{dataset}
-            WHERE code = {code}
-            """)
-        return retVal.fetchall()
+        cur.execute(f"""select value from DATASET_{dataset}
+            t max(date_created) from DATASET_{dataset} where code = '{code}')
+        and code = '{code}'""")
+        retVal = cur.fetchone()
+        con.close()
 
-    def getWithTimestamp(self,firstTimestamp,secondTimestamp,dataset):
-        retVal = self.connection.execute(f"""
-            SELECT *
-            FROM dataset{dataset}
-            WHERE timestamp BETWEEN '{firstTimestamp}' AND '{secondTImestamp}'
-            """)
-        return retVal.fetchall()
-               
-    def Deadband(self,oldValue,newValue):
-        deadband = abs(newValue-oldValue)/ oldValue * 100
-        return deadband
+        return retVal
+        
+   
+    
 
-    def Insert(self,id,code,value,dataset):
-        timestamp = datetime.now()
-        self.connection.execute(f"""
-            INSERT INTO dataset{dataset} (id,code,value,timestamp)
-            VALUES ({id},'{code}',{value},'{timestamp}')
-            """)
-        self.connection.commit()
+    def Insert(self,code,value,dataset):
+        con = sqlite3.connect('database.db')
+
+        cur = con.cursor()
+
+        dtime = datetime.now()
+        # Insert a row of data
+        cur.execute(f"""insert into DATASET_{dataset} (code, value) values ('{code}', {value})""")
+
+        con.commit()
+        con.close() 
